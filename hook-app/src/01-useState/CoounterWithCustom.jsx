2403fc05bc26968c6d462f4fbb150c74fb26c8f6
cristian lozano <yatogami2814@gmail.com>
2023-07-27 12:43:54 -0500

    utilizando el useState practicar mas

--- hook-app/src/01-useState/CoounterWithCustom.jsx
+++ hook-app/src/01-useState/CoounterWithCustom.jsx
@@ -1,15 +1,20 @@
 import { useCounter } from "./hooks/useCounter";
 
 export const CoounterWithCustom = () => {
-  
-const {counter} = useCounter()
-    return (
+  const { counter, increment, decrement, reset } = useCounter();
+  return (
     <>
       <h1>Coounter With Hook:{counter}</h1>
       <hr />
-      <button className="btn btn-primary">+1</button>
-      <button className="btn btn-primary">reset</button>
-      <button className="btn btn-primary">-1</button>
+      <button onClick={() => increment(1)} className="btn btn-primary">
+        +1
+      </button>
+      <button onClick={reset} className="btn btn-primary">
+        reset
+      </button>
+      <button onClick={() => decrement(1)} className="btn btn-primary">
+        -1
+      </button>
     </>
   );
 };
